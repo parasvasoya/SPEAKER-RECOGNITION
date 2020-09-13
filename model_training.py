dd7ef322851893b53b87a3ccc69e6c4e0494332e
parasvasoya <60727596+parasvasoya@users.noreply.github.com>
2020-09-13 20:34:37 +0530

    Update model_training.py

--- model_training.py
+++ model_training.py
@@ -8,49 +8,52 @@ from feature_extraction import extract_features
 import warnings
 import os
 import librosa
-warnings.filterwarnings("ignore")
+import sys
+warnings.filterwarnings(action="ignore",category=DeprecationWarning)
 DATASET_PATH = "DATASET"
 
 fhand=open("user_name.txt",mode='r+')
 features = np.asarray(())
 dest="user_models/"
-name_list=fhand.read()
-name_list=name_list.split()
-print(name_list)
-a="bharat"
+a="Megan"
+for dirpath,dirnames,filenames in os.walk(dest):
+    print(filenames)
+    temp_name=a+".gmm"
+    if temp_name in filenames:
+        print("already exist")
+        sys.exit()
+
+
+
 count = 1
 DATASET_PATH="DATASET/"+a
-if a not in name_list:
-    print(a)
-    fhand.write(a)
+for dirpath,dirnames,filenames in os.walk(DATASET_PATH):
+    for f in filenames:
+        file_path = os.path.join(dirpath, f)
+        #read audio file
+        sr,audio = read(file_path)
+        vector   = extract_features(audio,sr)
 
-    for dirpath,dirnames,filenames in os.walk(DATASET_PATH):
-        for f in filenames:
-            file_path = os.path.join(dirpath, f)
-            #read audio file
-            sr,audio = read(file_path)
-            vector   = extract_features(audio,sr)
+        if features.size == 0:
+            features = vector
+        else:
+            features = np.vstack((features, vector))
 
-            if features.size == 0:
-                features = vector
-            else:
-                features = np.vstack((features, vector))
+        if count==5:
+            gmm = gaussian_mixture.GaussianMixture(n_components = 16,max_iter = 200, covariance_type='diag',n_init = 3)
+            gmm.fit(features)
 
-            if count==5:
-                gmm = gaussian_mixture.GaussianMixture(n_components = 16,max_iter = 200, covariance_type='diag',n_init = 3)
-                gmm.fit(features)
+                # dumping the trained gaussian model
+            picklefile = a+".gmm"
+            print("model name ",picklefile)
+            file=open(dest+picklefile,mode="wb")
 
-                    # dumping the trained gaussian model
-                picklefile = a+".gmm"
-                print("model name ",picklefile)
-                file=open(dest+picklefile,mode="wb")
+            pickle.dump(gmm,file)
 
-                pickle.dump(gmm,file)
-                
-                print ('+ modeling completed for speaker:',picklefile," with data point = ",features.shape)
-                features = np.asarray(())
-                count = 0
+            print ('+ modeling completed for speaker:',picklefile," with data point = ",features.shape)
+            features = np.asarray(())
+            count = 0
 
-            count = count + 1
+        count = count + 1
 
 fhand.close()
