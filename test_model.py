9f50a07e870b2d05d381f7d75b7a00b126022ad7
Hemal Parmar <47689013+hemal5363@users.noreply.github.com>
2020-09-25 18:42:36 +0530

    Update test_model.py

--- test_model.py
+++ test_model.py
@@ -2,7 +2,7 @@ import os
 import pickle
 import numpy as np
 from scipy.io.wavfile import read
-from feature_extraction import  extract_features
+from feature_extraction import extract_features
 import warnings
 warnings.filterwarnings("ignore")
 import time
@@ -11,22 +11,23 @@ source="SampleData/"
 
 modelpath="user_models/"
 
-gmm_files=[os.path.join(modelpath,fname) for fname in os.listdir(modelpath) if fname.endswith('.gmm')]
 
-models=[pickle.load(open(fname,'rb')) for fname in gmm_files]
+def test_model(file_name):
 
-speakers=[fname.split('/')[-1].split('.gmm')[0] for fname in gmm_files]
+    gmm_files=[os.path.join(modelpath,fname) for fname in os.listdir(modelpath) if fname.endswith('.gmm')]
 
-error=0
-total_sample=0.0
+    models=[pickle.load(open(fname,'rb')) for fname in gmm_files]
 
-print("do you want to test a single audio : press 1 or complete press 0")
-take=int(input().strip())
+    speakers=[fname.split('/')[-1].split('.gmm')[0] for fname in gmm_files]
 
-if take==1:
-    print("enter the file name")
-    path=input().strip()
-    print("testing audio",path)
+    # print("do you want to test a single audio : press 1 or complete press 0")
+    # take=int(input().strip())
+
+    # if take==1:
+    # print("enter the file name")
+    # path=input().strip()
+    # print("testing audio",path)
+    path = file_name
     sr,audio=read(source+path)
     vector=extract_features(audio,sr)
 
@@ -38,13 +39,13 @@ if take==1:
         log_likelihood[i]=scores.sum()
     max1 = max(log_likelihood)
     if max1 < -30:
-        print("you are not ragister...")
+        return "you are not ragister..."
     # winner=np.argmax(log_likelihood)
     else:
         winner = np.where(log_likelihood == max1)
-        print("detected as -",speakers[winner[0][0]])
+        return speakers[winner[0][0]]
 
-    time.sleep(1.0)
+    # time.sleep(1.0)
 
-elif take==0:
-    print("we are in 0 part")
+# elif take==0:
+#     print("we are in 0 part")
